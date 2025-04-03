af74af12261a4cc14e24da88d90e4f4f6d9cda1e
tjdgusghkd <143480500+tjdgusghkd@users.noreply.github.com>
2025-04-03 15:15:11 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        if(arr[0] == arr[1] && arr[1] == arr[2]) {
+            System.out.println(10000 + arr[0] * 1000);
+        } else if(arr[0] == arr[1] || arr[1] == arr[2]) {
+            System.out.println(1000 + arr[1] * 100);
+        } else {
+            System.out.println(arr[2] * 100);
+        }
+    }
+}
