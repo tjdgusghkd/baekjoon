90eccf51dc2923c2541c8ee14fb33abd0d45b7f4
tjdgusghkd <143480500+tjdgusghkd@users.noreply.github.com>
2026-01-27 22:16:40 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<String>();
+        for(int i = 0; i <todo_list.length; i++) {
+            if(!finished[i]){
+                list.add(todo_list[i]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
