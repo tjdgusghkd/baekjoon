28dd639a65d16878ae1bc1642280d64d75ff9877
tjdgusghkd <143480500+tjdgusghkd@users.noreply.github.com>
2026-03-13 00:18:44 +0900

    [Bronze III] Title: 최소, 최대, Time: 2224 ms, Memory: 323144 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,29 +1,19 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-
-        for(int i = 0; i < arr.length; i++) {
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
-        int min = arr[0];
-        int max = arr[0];
-        
-        for(int i = 0; i < arr.length; i++) {
+        Arrays.sort(arr);
 
-            if(arr[i] < min) {
-                min = arr[i];
-            }
-            if(arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        System.out.println(min + " " + max);
+        System.out.printf("%d %d",arr[0], arr[arr.length-1]);
+    
 
     }
 }
