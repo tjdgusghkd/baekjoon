c3c261c5fc19c6b91eb5caf16a4baf5a51b106c9
tjdgusghkd <143480500+tjdgusghkd@users.noreply.github.com>
2026-04-22 22:04:04 +0900

    [level 2] Title: 가장 큰 수, Time: 253.64 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] str = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            str[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(str[0].equals("0")){
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(String s : str){
+            sb.append(s);
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
